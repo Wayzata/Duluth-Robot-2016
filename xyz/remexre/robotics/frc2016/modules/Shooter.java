96cde556065e84a74197d696987b79cac4d536ff
Mariel <mariel21279@gmail.com>
2016-02-06 00:13:44 -0600

    added changes to the shooter

--- xyz/remexre/robotics/frc2016/modules/Shooter.java
+++ xyz/remexre/robotics/frc2016/modules/Shooter.java
@@ -1,15 +1,76 @@
 package xyz.remexre.robotics.frc2016.modules;
 
+import edu.wpi.first.wpilibj.CANTalon;
+import xyz.remexre.robotics.frc2016.RobotParts;
+
+/**
+ * The controlling class for the Shooter
+ * @author mariel
+ */
 public class Shooter {
-	public void raiseArm() { /* TODO */ }
-	public void lowerArm() { /* TODO */ }
-	public void stopArm() { /* TODO */ }
-	
-	public void pullBelt() { /* TODO */ }
-	public void pushBelt() { /* TODO */ }
-	public void stopBelt() { /* TODO */ }
-	
+
+	private CANTalon mainMotor;
+	private CANTalon beltMotor;
+	private CANTalon armMotor;
+
+	/**
+	 * Constructs the various shooter motors
+	 */
+	public Shooter() {
+		this.mainMotor = new CANTalon(RobotParts.MOTORS.SHOOTER_MAIN);
+		this.beltMotor = new CANTalon(RobotParts.MOTORS.SHOOTER_BELT);
+		this.armMotor = new CANTalon(RobotParts.MOTORS.SHOOTER_ARM);
+	}
+
+	/**
+	 * raises the arm
+	 */
+	public void raiseArm() {
+		this.armMotor.set(1);
+	}
+
+	/**
+	 * lowers the arm
+	 */
+	public void lowerArm() {
+		this.armMotor.set(-1);
+	}
+
+	/**
+	 * Pauses and stops the arm from moving
+	 */
+	public void stopArm() {
+		this.armMotor.set(0);
+	}
+
+	/**
+	 * This either goes counter cw or ccw, i am unsure
+	 */
+	public void pullBelt() {
+		this.beltMotor.set(1);
+	}
+
+	/**
+	 * This either goes counter cw or ccw, i am unsure
+	 */
+	public void pushBelt() {
+		this.beltMotor.set(-1);
+	}
+
+	/**
+	 * pauses the belt motor
+	 */
+	public void stopBelt() {
+		this.beltMotor.set(0);
+	}
+
+	/**
+	 * main motor in robot shooter spins and shoots the ball
+	 */
 	public boolean shoot() {
+
+		this.mainMotor.set(1);
+
 		// TODO
 		return false;
 	}
